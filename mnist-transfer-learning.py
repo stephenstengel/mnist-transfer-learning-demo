fbb710544ef73cc973b5e42b89bd0e23b9772fe7
Stephen <stephen.stengel@cwu.edu>
2022-04-13 18:07:34 -0700

    Initial commit.

---
+++ mnist-transfer-learning.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  mnist-transfer-learning.py
+#  
+#  2022 Stephen Stengel <stephen.stengel@cwu.edu>
+
+#A test program to see how transfer learning works.
+#Tutorial sources:
+# ~ https://keras.io/guides/transfer_learning/
+# ~ https://towardsdatascience.com/transfer-learning-using-pre-trained-alexnet-model-and-fashion-mnist-43898c2966fb?gi=1f9cc1728578
+# ~ https://www.kaggle.com/code/muerbingsha/mnist-vgg19/notebook
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+def main(args):
+	print("Hello lol!")
+
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
