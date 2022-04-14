d035573ed83b426c8ac61e045d7cd028d8d4efc6
Stephen <stephen.stengel@cwu.edu>
2022-04-13 19:34:33 -0700

    Add last of hypothetical bits.

--- mnist-transfer-learning.py
+++ mnist-transfer-learning.py
@@ -17,6 +17,9 @@ print("Running imports...")
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from keras.datasets import mnist		#images of digits.
+from keras.datasets import fashion_mnist  #images of clothes
+from keras.datasets import cifar10    #small images
 
 print("Done!")
 
@@ -94,10 +97,97 @@ def main(args):
 
 	print("\n\n\n\n#########################################################\n\n\n")
 	
+	print("Now for the actual example using pretrained weights.\n\n")
+	
+	base_model = keras.applications.Xception(
+		weights='imagenet',  # Load weights pre-trained on ImageNet.
+		input_shape=(150, 150, 3),
+		include_top=False)  # Do not include the ImageNet classifier at the top.
+
+	base_model.trainable = False
+	
+	#New model to go on top of base_model
+	inputs = keras.Input(shape=(150, 150, 3))
+	# We make sure that the base_model is running in inference mode here,
+	# by passing `training=False`. This is important for fine-tuning, as you will
+	# learn in a few paragraphs.
+	x = base_model(inputs, training=False)
+	# Convert features of shape `base_model.output_shape[1:]` to vectors
+	x = keras.layers.GlobalAveragePooling2D()(x)
+	# A Dense classifier with a single unit (binary classification)
+	outputs = keras.layers.Dense(1)(x)
+	model = keras.Model(inputs, outputs)
+	
+	model.compile(
+		optimizer=keras.optimizers.Adam(),
+		loss=keras.losses.BinaryCrossentropy(from_logits=True),
+		metrics=[keras.metrics.BinaryAccuracy()])
+
+#This part needs a new dataset loaded. I'll look at the rest of the tutorial first.
+	# ~ model.fit(new_dataset, epochs=20, callbacks=..., validation_data=...)
+	
+	print("\n\n\n\n#########################################################")
+	print("Fine tuning...")
+
+	# Unfreeze the base model
+	base_model.trainable = True
+	
+	# It's important to recompile your model after you make any changes
+	# to the `trainable` attribute of any inner layer, so that your changes
+	# are take into account
+	model.compile(
+		optimizer=keras.optimizers.Adam(1e-5),  # Very low learning rate
+		loss=keras.losses.BinaryCrossentropy(from_logits=True),
+		metrics=[keras.metrics.BinaryAccuracy()])
+	
+	# Train end-to-end. Be careful to stop before you overfit!
+	# ~ model.fit(new_dataset, epochs=10, callbacks=..., validation_data=...)
+	
+	#BIG NOTE: Whenever you change the trainable or other stuff in the
+	#model, you must re-compile it otherwise it wont do much.
+
+	print("\n\n\n\n#########################################################")
+	print("If using a custom fit method instead of .fit()...")
+
+	# Create base model
+	base_model = keras.applications.Xception(
+		weights='imagenet',
+		input_shape=(150, 150, 3),
+		include_top=False)
+	# Freeze base model
+	base_model.trainable = False
+	
+	# Create new model on top.
+	inputs = keras.Input(shape=(150, 150, 3))
+	x = base_model(inputs, training=False)
+	x = keras.layers.GlobalAveragePooling2D()(x)
+	outputs = keras.layers.Dense(1)(x)
+	model = keras.Model(inputs, outputs)
+	
+	loss_fn = keras.losses.BinaryCrossentropy(from_logits=True)
+	optimizer = keras.optimizers.Adam()
+	
+	# Iterate over the batches of a dataset.
+	# ~ for inputs, targets in new_dataset:
+		# ~ # Open a GradientTape.
+		# ~ with tf.GradientTape() as tape:
+			# ~ # Forward pass.
+			# ~ predictions = model(inputs)
+			# ~ # Compute the loss value for this batch.
+			# ~ loss_value = loss_fn(targets, predictions)
+	
+	# Get gradients of loss wrt the *trainable* weights.
+	# ~ gradients = tape.gradient(loss_value, model.trainable_weights)
+	# ~ # Update the weights of the model.
+	# ~ optimizer.apply_gradients(zip(gradients, model.trainable_weights))
 	
 	
+	print("\n\n\n\n#########################################################")
+
+
 
 	return 0
+
 
 if __name__ == '__main__':
 	import sys
